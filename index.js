4846529a47a680de38e0a57e2791b31f6cf677b1
Akash <hafejmohammadakash@gmail.com>
2025-07-22 13:48:29 +0600

    mongodb setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// midleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Bistro Boos Is Sitting')
+});
+
+app.listen(port, () => {
+    console.log(`Bistro Boos is on the port: ${port}`)
+})

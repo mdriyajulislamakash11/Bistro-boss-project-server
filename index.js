dd14a0d5ef5ce7d448664d64524da8ef1c9bcd82
Akash <hafejmohammadakash@gmail.com>
2025-08-04 13:53:14 +0600

    a,o cart delete er kaj korlam

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // mongo DB
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zchez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -53,6 +53,13 @@ async function run() {
       const result = await cartsCollection.insertOne(cartItems);
       res.send(result);
     });
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartsCollection.deleteOne(query)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });

a47d657b0da40ec15311c5a651743066fd218870
Akash <hafejmohammadakash@gmail.com>
2025-08-11 12:50:11 +0600

    menu delete er kaj korlam

--- index.js
+++ index.js
@@ -152,6 +152,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await menuCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // CARTS Collections:        \\________________________ Cart ______________________//
 
     app.get("/carts", async (req, res) => {

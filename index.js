278a26ccf5ff194df057daf37d328b657ef3c9f4
Akash <hafejmohammadakash@gmail.com>
2025-08-12 18:51:01 +0600

    admin stats er kaj korlam pipline diye

--- index.js
+++ index.js
@@ -255,6 +255,41 @@ async function run() {
       res.send({ paymentResult, deletedResult });
     });
 
+    //admin stats:
+    app.get("/admin-stats", verifyToken, verifyAdmin, async (req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const menuItems = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentsCollection.estimatedDocumentCount();
+
+      // this is not the best way
+      // const payments = await paymentsCollection.find().toArray();
+      // const revenue = payments.reduce((total, payment) => total + payment.price , 0);
+      // const newRevenue = revenue.toFixed(2);
+
+      // this is the best way:
+      const result = await paymentsCollection
+        .aggregate([
+          {
+            $group: {
+              _id: null,
+              totalRevenue: {
+                $sum: "$price",
+              },
+            },
+          },
+        ])
+        .toArray();
+        const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+
+      res.send({
+        users,
+        menuItems,
+        orders,
+        // newRevenue
+        revenue
+      });
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
